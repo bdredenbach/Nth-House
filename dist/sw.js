38911e44456126b5bfd2a0fea5652ccb9ae9d26d
bdredenbach <bdredenbach@gmail.com>
2026-09-12 02:56:57 -0400

    Delete dist/sw.js

--- dist/sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="nth-house-v1";
-const CORE=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];
-self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE))));
-self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
-self.addEventListener("fetch",event=>event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{if(event.request.method==="GET"){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));}return response;}))));
